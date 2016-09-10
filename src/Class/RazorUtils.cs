208e97b2b5a56dd216ae3e207d868ecd91d0e1f3
Fabrício Zimmerer Murta <fabricio@object.net>
2016-09-10 03:03:30 -0300

    Reviews all examples to use better RazorView syntax.

---
+++ src/Class/RazorUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ext.Net.Mobile.MVC
+{
+    public class RazorUtils
+    {
+        /// <summary>
+        /// This allows returning a plain string out of a @<text></text> block.
+        /// </summary>
+        /// <param name="?"></param>
+        public static string StringifyTextBlock(Func<object, object> contents)
+        {
+            return contents(null).ToString();
+        }
+    }
+}
